76afd7403d6bde914f9ca199a97f5ccf6fd4d99f
Aditya Naulakha <naulakhaaditya4@gmail.com>
2025-03-17 00:40:25 +0530

    Loader fixed

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
       setTimeout(() => {
         setLoading(false);
       }, 500); // Extra time for smooth fade-out
-    }, 2500); // Loader visible for 2 seconds
+    }, 2500); // Loader visible for 2.5 seconds
   }, []);
 
   return (
