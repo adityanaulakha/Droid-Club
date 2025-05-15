1eabf6aed5646cf558a1574e9331caebb5a2d439
adityanaulakha <naulakhaaditya4@gmail.com>
2025-05-16 00:11:11 +0530

    Contact Us added, removed ClickSpark

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -3,6 +3,7 @@ import Home from "../pages/Home";
 // import About from "../pages/About";
 // import Events from "../pages/Events";
 // import Teams from "../pages/Teams";
+import ContactUs from "../pages/ContactUs";
 
 const AppRouter = () => {
   return (
@@ -11,6 +12,7 @@ const AppRouter = () => {
       {/* <Route path="/about" element={<About />} /> */}
       {/* <Route path="/events" element={<Events />} /> */}
       {/* <Route path="/teams" element={<Teams />} /> */}
+      <Route path="/contact-us" element={<ContactUs />} />
     </Routes>
   );
 };
