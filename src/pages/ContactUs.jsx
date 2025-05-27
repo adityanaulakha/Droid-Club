2a1a35462026893778b8071ab94d92c5b4ceb70a
adityanaulakha <naulakhaaditya4@gmail.com>
2025-05-27 17:41:51 +0530

    Contact Form Tweaked

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -2,7 +2,7 @@ import ContactForm from "../components/Home/ContactForm";
 
 export default function ContactUs() {
   return (
-    <div className="min-h-screen flex items-center justify-center p-4">
+    <div>
       <ContactForm />
     </div>
   );
